b08a3d271aae91e57c879690a20cbb7a0190d401
octavioegeafaure <octavioegeaf@gmail.com>
2022-08-04 15:58:14 -0300

    Cards categorias con grid

--- FrontEnd/proyecto-integrador/src/Paginas/Home/Categorias/Cards.js
+++ FrontEnd/proyecto-integrador/src/Paginas/Home/Categorias/Cards.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import Item from "./Item"
-import data from "./data.json"
-import "./Cards.css"
-import "./Item.css";
+import React from "react";
+// import Item from "./Item"
+import data from "./data.json";
+import "./Cards.css";
+import { Grid, ImageList, ImageListItem } from "@mui/material";
+// xs mobile sm tablet md desktop xl larger monitors 
 export default function Cards() {
 
   return (
-    <div className="categorias">
-      {
-        data.map((i) => 
-        <Item 
-            key={i.id} 
-            categoria={i}  
-        />
-        )}
-    </div>
+    <Grid container spacing={1} >
+      {data.map((categoria) => (
+        <Grid item xs={12} sm={6} md={3} className="categoria-card" >
+          <img
+            key={categoria.id}
+            src={categoria.categoria.imagen}
+            alt=""
+            className="categoria-card-img"
+          ></img>
+        <Grid>
+          <h2 className="categoria-card-nombre">{categoria.categoria.nombre}</h2>
+        </Grid>
+        <Grid>
+          <p className="categoria-card-descripcion">{categoria.categoria.descripcion}</p>
+        </Grid> 
+        </Grid>
+      ))}
+    </Grid>
   );
 }
-
