603967f4fb9a4078a4a3c7be846b70e1c4a83041
Daniel Villajuan <daniel.villajuan@bigbox.com.ar>
2022-08-15 22:37:54 -0300

    feat login , TL no estuvo aqui

---
+++ FrontEnd/proyecto-integrador/src/Context/AuthContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(JSON.parse(localStorage.getItem("user")));
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
