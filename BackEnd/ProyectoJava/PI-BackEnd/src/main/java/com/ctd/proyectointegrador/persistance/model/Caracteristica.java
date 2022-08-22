9a213d305c6a43f7f13c29751b7639788b91db7a
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-08-22 15:16:58 -0300

    relacion producto-categoria

---
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/persistance/model/Caracteristica.java
@@ -0,0 +1,54 @@
+package com.ctd.proyectointegrador.persistance.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
+public class Caracteristica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    public Caracteristica() {
+    }
+
+    public Caracteristica(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Caracteristica{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
