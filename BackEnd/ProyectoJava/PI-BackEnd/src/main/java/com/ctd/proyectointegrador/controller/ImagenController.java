93a11d2f3f2abab981d868d710ccbe40cd722f29
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-08-23 15:21:38 -0300

    fix back

--- BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/controller/ImagenController.java
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/controller/ImagenController.java
@@ -16,37 +16,35 @@ public class ImagenController {
     @Autowired
     ImagenService imagenService;
 
-    @Autowired
-    Map<String, Object> response = new HashMap<>();
     private Integer codigo;
 
     @PostMapping()
     public ResponseEntity<Map<String, Object>> guardar(@RequestBody ImagenDTO imagenes){
-        response = imagenService.guardar(imagenes);
+        Map<String, Object> response = imagenService.guardar(imagenes);
         return ResponseEntity.created(URI.create("/Imagenes")).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Map<String, Object>> buscar(@PathVariable Integer id) {
-        response = imagenService.buscar(id);
+        Map<String, Object> response = imagenService.buscar(id);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
     @PutMapping("/{id}")
     public ResponseEntity<Map<String, Object>> actualizar(@PathVariable Integer id, @RequestBody ImagenDTO i) {
-        response = imagenService.actualizar(id, i);
+        Map<String, Object> response = imagenService.actualizar(id, i);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> eliminar(@PathVariable Integer id) {
-        response = imagenService.eliminar(id);
+        Map<String, Object> response = imagenService.eliminar(id);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
     @GetMapping()
     public ResponseEntity<Map<String, Object>> listarTodos(){
-        response = imagenService.listarTodos();
+        Map<String, Object> response = imagenService.listarTodos();
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
--- BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/controller/ProductoController.java
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/controller/ProductoController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,41 +15,38 @@ public class ProductoController {
     @Autowired
     ProductoService productoService;
 
-    @Autowired
-    Map<String, Object> response = new HashMap<>();
-
     private Integer codigo;
 
     @PostMapping()
     public ResponseEntity<Map<String, Object>> guardar(@RequestBody ProductoDTO producto){
-        response = productoService.guardar(producto);
+        Map<String, Object> response = productoService.guardar(producto);
         return ResponseEntity.created(URI.create("/productos")).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Map<String, Object>> buscar(@PathVariable Integer id) {
-        response = productoService.buscar(id);
+        Map<String, Object> response = productoService.buscar(id);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Map<String, Object>> actualizar(@PathVariable Integer id, @RequestBody ProductoDTO p) {
-        response = productoService.actualizar(id, p);
+        Map<String, Object> response = productoService.actualizar(id, p);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> eliminar(@PathVariable Integer id) {
-        response = productoService.eliminar(id);
+        Map<String, Object> response = productoService.eliminar(id);
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
 
     @GetMapping()
     public ResponseEntity<Map<String, Object>> listarTodos(){
-        response = productoService.listarTodos();
+        Map<String, Object> response = productoService.listarTodos();
         codigo = (Integer) response.get("codigo");
         return ResponseEntity.status(codigo).body(response);
     }
