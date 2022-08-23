3876a161ef6bea75162adeb210f5e65675eeac93
Stephania Martheyn <stephaniemartheyn@gmail.com>
2022-08-23 05:10:48 +0000

    Se agrego el servicio de productos aleatorios para usuarios no registrados

--- BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/service/impl/ProductoService.java
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/service/impl/ProductoService.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Service
 public class ProductoService implements IService<ProductoDTO> {
@@ -48,8 +50,7 @@ public class ProductoService implements IService<ProductoDTO> {
         return buildResponse(mapper.convertValue(prodRespuesta, ProductoDTO.class), "producto creado", 201);
     }
 
-
-    public Map<String, Object> buscar(Integer id){
+    public Map<String, Object> buscar(Integer id) {
         Producto prodRespuesta = productoRepository.findById(id).get();
         return buildResponse(mapper.convertValue(prodRespuesta, ProductoDTO.class), "producto encontrado", 201);
     }
@@ -58,13 +59,13 @@ public class ProductoService implements IService<ProductoDTO> {
         Producto actualizar = mapper.convertValue(object, Producto.class);
         Producto productoEnBD = productoRepository.findById(id).orElse(null);
         if (productoEnBD == null) {
-            //error
+            // error
             return buildResponse(new ProductoDTO(), "No existe id " + id, 404);
         }
         productoEnBD.setTitulo(actualizar.getTitulo());
         productoEnBD.setDescripcion(actualizar.getDescripcion());
         productoEnBD.setImagenes(actualizar.getImagenes());
-/*        productoEnBD.setCaracteristicas(actualizar.getCaracteristicas());*/
+        /* productoEnBD.setCaracteristicas(actualizar.getCaracteristicas()); */
         productoEnBD.setCiudad(ciudadRepository.findById(actualizar.getCiudad().getId()).get());
         Producto prodRespuesta = productoRepository.save(productoEnBD);
 
@@ -83,6 +84,21 @@ public class ProductoService implements IService<ProductoDTO> {
     public Map<String, Object> listarTodos() {
 
         List<Producto> listaProd = productoRepository.findAll();
+        List<ProductoDTO> listaDTO = new ArrayList<>();
+
+        for (Producto p : listaProd) {
+            ProductoDTO prodDTO = mapper.convertValue(p, ProductoDTO.class);
+            listaDTO.add(prodDTO);
+        }
+        return buildResponse(listaDTO, "lista creada", 200);
+
+    }
+
+    public Map<String, Object> listarProductosAleatorios() {
+        Random r = new Random();
+        r.setSeed(System.currentTimeMillis());
+        List<Producto> listaProd = productoRepository.findAll();
+        Collections.shuffle(listaProd, r);
         List<ProductoDTO> listaDTO = new ArrayList<>();
 
         for (Producto p : listaProd) {
