d5217e20f3669c7f15e1c1a849c9217a6a24b33f
Belenrisso <belerisso@gmail.com>
2022-08-24 19:27:47 -0300

    service fix

--- BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/service/impl/CategoriaService.java
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/service/impl/CategoriaService.java
@@ -19,108 +19,59 @@ public class CategoriaService implements IService<CategoriaDTO> {
     @Autowired
     ObjectMapper mapper;
 
-    Map<String, Object> respuesta = new HashMap<>();
+    private Map<String, Object> buildResponse(Object dto, String message, Integer code) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("productos", dto);
+        response.put("message", message);
+        response.put("codigo", code);
+        return response;
+    }
 
     public Map<String, Object> guardar(CategoriaDTO c){
-        // Uno de los problemas que surgieron fue que cuando se realizaban varias consultas,
-        // quedaban cargados en el HashMap los pares de clave:valor de cada una. response.clear() es citado
-        // al comienzo de cada metodo para eliminar lo que se encuentre previamente y asi tener el HashMap limpio.
-        respuesta.clear();
-        // Convierto CategoriaDTO a Categoria
         Categoria categoria = mapper.convertValue(c, Categoria.class);
-        // Guardo Categoria en la base de datos
         Categoria nuevaCat = categoriaRepository.save(categoria);
-        // Preparo respuesta y transformo a DTO
-        respuesta.put("codigo", 200);
-        respuesta.put("categoria", mapper.convertValue(nuevaCat, CategoriaDTO.class));
-        return respuesta;
+        return buildResponse(mapper.convertValue(nuevaCat, CategoriaDTO.class), "Categoria guardada",201);
     }
 
     public Map<String, Object> buscar(Integer id){
-
-        respuesta.clear();
-        // Verifico que exista el id demandado
-        if(categoriaRepository.findById(id).isPresent()){
-            // Obtengo Categoria
-            Categoria categoria = categoriaRepository.findById(id).get();
-            // Preparo HashMap de respuesta de consulta satisfactoria.
-            respuesta.put("codigo", 200);
-            respuesta.put("categoria", mapper.convertValue(categoria, CategoriaDTO.class));
-        } else {
-            // Si el objeto respuesta tiene una categoria cargada por consulta realizada previamente, la elimino
-            // del objeto de respuesta
-            respuesta.remove("categoria");
-            // Preparo HashMap de respuesta de consulta erronea.
-            respuesta.put("codigo", 404);
-            respuesta.put("mensaje", "Categoria id: " + id + " no existe");
-        }
-        // Retorno HashMap de respuesta
-        return respuesta;
+        Categoria categoria = categoriaRepository.findById(id).get();
+        return buildResponse(mapper.convertValue(categoria, CategoriaDTO.class),"Categoria id "+ id + "encontrada",201);
     }
+
 
     public Map<String, Object> actualizar(Integer id, CategoriaDTO object){
-
-        respuesta.clear();
-        // Reutilizo el metodo buscar para obtener la categoria (La obtengo como DTO)
-        respuesta = buscar(id);
-        // Verifico que exista el id demandado
-        if(categoriaRepository.findById(id).isPresent()) {
-            // Transformo la categoria obtenida anteriormente de DTO a Categoria
-            // .get("clave") me retorna el valor que almacena dicha clave
-            Categoria c = mapper.convertValue(respuesta.get("categoria"), Categoria.class);
-            // Actualizacion de la categoria. Si uno de los atributos del objeto entrante es nulo, queda con su valor
-            // actual. Si no es nulo, es porque el cliente me envia el nuevo valor de dicho atributo,
-            // entonces se lo asigno
-            c.setTitulo(object.getTitulo() != null ?  object.getTitulo() : c.getTitulo());
-            c.setDescripcion(object.getDescripcion() != null ?  object.getDescripcion() : c.getDescripcion());
-            c.setUrl(object.getUrl() != null ?  object.getUrl() : c.getUrl());
-            // Envio los cambios
-            categoriaRepository.save(c);
-            // En la respuesta, reemplazo la categoria con valores viejos por la nueva categoria (Transformado
-            // a DTO)
-            respuesta.replace("categoria", mapper.convertValue(c, CategoriaDTO.class));
-        }
-        // Retorno HashMap de respuesta
-        return respuesta;
+     Categoria actualizar = mapper.convertValue(object, Categoria.class);
+     Categoria categoriaenBD =categoriaRepository.findById(id).orElse(null);
+     if (categoriaenBD == null){
+         return buildResponse(new CategoriaDTO(), "No existe la categoria con id "+ id,404);
+     }
+     categoriaenBD.setTitulo(actualizar.getTitulo());
+     categoriaenBD.setDescripcion(actualizar.getDescripcion());
+     categoriaenBD.setUrl(actualizar.getUrl()); //lista de prod?
+     Categoria categoriar = categoriaRepository.save(categoriaenBD);
+     return buildResponse(mapper.convertValue(categoriar, CategoriaDTO.class), "Actualizacion exitosa", 201);
     }
+    
 
     public Map<String, Object> eliminar(Integer id) {
 
-        respuesta.clear();
-        // Verifico que exista el id demandado
         if (categoriaRepository.findById(id).isPresent()) {
-            // Elimino la categoria indicada de la base de datos
             categoriaRepository.deleteById(id);
-            // Preparo HashMap de respuesta de consulta satisfactoria.
-            respuesta.put("codigo", 200);
-            respuesta.put("mensaje","Categoria id: " + id + " eliminada");
+            return buildResponse(new CategoriaDTO(), "Categoria id "+ id + " eliminada", 201);
         } else {
-            // Preparo HashMap de respuesta de consulta erronea.
-            respuesta.put("codigo", 404);
-            respuesta.put("mensaje","Categoria id: " + id + " no existe");
+            return buildResponse(new CategoriaDTO(), "Categoria id "+ id + " no existe", 404);
         }
-        return respuesta;
     }
 
-    public Map<String, Object> listarTodos(){
 
-        respuesta.clear();
-        // Obtengo todos los registros de la tabla Categorias.
+    public Map<String, Object> listarTodos(){
         List<Categoria> listaCat = categoriaRepository.findAll();
-        // Creo un Array donde se almacenaran las categorias transformadas a DTO
         List<CategoriaDTO> listaDTO = new ArrayList<>();
 
-        // Bucle for que recorre la lista de Categorias
         for (Categoria c: listaCat){
-            // A cada categoria de listaCat la transformo a DTO
             CategoriaDTO catDTO = mapper.convertValue(c, CategoriaDTO.class);
-            // Almaceno la categoria transformada a DTO en Array creado previamente
             listaDTO.add(catDTO);
         }
-        respuesta.put("codigo", 200);
-        respuesta.put("categorias", listaDTO);
-        //Retorno Array con categorias en DTO
-        return respuesta;
-
+        return buildResponse(listaDTO, "Lista creada",200);
     }
 }
