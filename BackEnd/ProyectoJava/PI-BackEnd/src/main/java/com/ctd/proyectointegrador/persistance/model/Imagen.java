0a53ce7189c0c45c97fa7f68fe5af0e7387eb297
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-08-24 13:11:43 -0300

    update info db

--- BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/persistance/model/Imagen.java
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/java/com/ctd/proyectointegrador/persistance/model/Imagen.java
@@ -1,4 +1,5 @@
 package com.ctd.proyectointegrador.persistance.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,11 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name = "imagenes")
 public class Imagen {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +23,6 @@ public class Imagen {
     private String url;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "producto_id")
+    @JsonIgnore
     private Producto producto;
 }
--- BackEnd/ProyectoJava/PI-BackEnd/src/main/resources/import.sql
+++ BackEnd/ProyectoJava/PI-BackEnd/src/main/resources/import.sql
@@ -1,28 +1,46 @@
-INSERT INTO categoria VALUES (null, 'velocidad y desempeño', 'Deportivos', 'https://images.unsplash.com/photo-1588127333419-b9d7de223dcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80');
-INSERT INTO categoria VALUES (null, 'detallado, refinado y caro',  'De Lujo', 'https://images.unsplash.com/photo-1616789916437-bbf724d10dae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
-INSERT INTO categoria VALUES (null, 'espacioso y comodo para grandes y niños',  'Familiares', 'https://images.unsplash.com/photo-1648902180388-e52c0fdebcd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80');
-INSERT INTO categoria VALUES (null, 'espacioso y comodo para grandes y niños',  'Economicos', 'https://images.unsplash.com/photo-1639640163962-9e845bd7294a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
+INSERT INTO categorias VALUES (null, 'velocidad y desempeño', 'Deportivos', 'https://images.unsplash.com/photo-1588127333419-b9d7de223dcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80');
+INSERT INTO categorias VALUES (null, 'detallado, refinado y caro',  'De Lujo', 'https://images.unsplash.com/photo-1616789916437-bbf724d10dae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
+INSERT INTO categorias VALUES (null, 'espacioso y comodo para grandes y niños',  'Familiares', 'https://images.unsplash.com/photo-1648902180388-e52c0fdebcd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80');
+INSERT INTO categorias VALUES (null, 'espacioso y comodo para grandes y niños',  'Economicos', 'https://images.unsplash.com/photo-1639640163962-9e845bd7294a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
 
-INSERT INTO ciudad VALUES (null, 'Mendoza', 'Argentina', 'Mendoza');
-INSERT INTO ciudad VALUES (null, 'Cordoba', 'Argentina', 'Cordoba');
-INSERT INTO ciudad VALUES (null, 'Bogota', 'Colombia', 'Cundinamarca');
-INSERT INTO ciudad VALUES (null, 'Medellin', 'Colombia', 'Antioquia ');
+INSERT INTO ciudades VALUES (null, 'Mendoza', 'Argentina', 'Mendoza');
+INSERT INTO ciudades VALUES (null, 'Cordoba', 'Argentina', 'Cordoba');
+INSERT INTO ciudades VALUES (null, 'Bogota', 'Colombia', 'Cundinamarca');
+INSERT INTO ciudades VALUES (null, 'Medellin', 'Colombia', 'Antioquia ');
 
-INSERT INTO producto VALUES (null, 'El Dodge Charger es un automóvil deportivo producido por el fabricante estadounidense Dodge, división del grupo Stellantis. A la fecha, existen diferentes generaciones en tres diferentes plataformas.', 'Dodge Charger', 1, 1);
-INSERT INTO producto VALUES (null, 'El Lamborghini Veneno es un automóvil superdeportivo de 2 puertas de tijera biplaza, con motor central-trasero montado longitudinalmente y tracción en las cuatro ruedas, producido por fabricante italiano Lamborghini.', 'Lamborghini Veneno', 2, 2);
-INSERT INTO producto VALUES (null, 'El Ford Fiesta es un automóvil de turismo del segmento B desarrollado por la filial europea de la empresa estadounidense Ford Motor Company, y fabricado en Alemania, España, Brasil, México, China, India y Sudáfrica.', 'Ford Fiesta', 3, 3);
-INSERT INTO producto VALUES (null, 'El Fiat 600 fue un pequeño automóvil diseñado por Dante Giacosa y construido por la empresa italiana Fiat desde 1955 hasta 1969. Fue un éxito mundial que se fabricó en muchos países, en la mayoría de ellos bajo la marca Fiat.', 'Fiat 600', 4, 4);
+INSERT INTO productos VALUES (null, 'El Dodge Charger es un automóvil deportivo producido por el fabricante estadounidense Dodge, división del grupo Stellantis. A la fecha, existen diferentes generaciones en tres diferentes plataformas.', 'Dodge Charger', 1, 1);
+INSERT INTO productos VALUES (null, 'El Lamborghini Veneno es un automóvil superdeportivo de 2 puertas de tijera biplaza, con motor central-trasero montado longitudinalmente y tracción en las cuatro ruedas, producido por fabricante italiano Lamborghini.', 'Lamborghini Veneno', 2, 2);
+INSERT INTO productos VALUES (null, 'El Ford Fiesta es un automóvil de turismo del segmento B desarrollado por la filial europea de la empresa estadounidense Ford Motor Company, y fabricado en Alemania, España, Brasil, México, China, India y Sudáfrica.', 'Ford Fiesta', 3, 3);
+INSERT INTO productos VALUES (null, 'El Fiat 600 fue un pequeño automóvil diseñado por Dante Giacosa y construido por la empresa italiana Fiat desde 1955 hasta 1969. Fue un éxito mundial que se fabricó en muchos países, en la mayoría de ellos bajo la marca Fiat.', 'Fiat 600', 4, 4);
 
-INSERT INTO imagen VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 1);
-INSERT INTO imagen VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 1);
-INSERT INTO imagen VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 1);
-INSERT INTO imagen VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 1);
-INSERT INTO imagen VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 1);
+INSERT INTO imagenes VALUES (null, 'https://www.autonocion.com/wp-content/uploads/2021/02/dodge-charger6-930x639.jpg', 1);
+INSERT INTO imagenes VALUES (null, 'https://www.autonocion.com/wp-content/uploads/2021/02/dodge-charger6-930x639.jpg', 1);
+INSERT INTO imagenes VALUES (null, 'https://www.autonocion.com/wp-content/uploads/2021/02/dodge-charger6-930x639.jpg', 1);
+INSERT INTO imagenes VALUES (null, 'https://www.autonocion.com/wp-content/uploads/2021/02/dodge-charger6-930x639.jpg', 1);
+INSERT INTO imagenes VALUES (null, 'https://www.autonocion.com/wp-content/uploads/2021/02/dodge-charger6-930x639.jpg', 1);
 
-INSERT INTO caracteristica VALUES (null, 'Transmision Automatica', 'urlprueba.com');
-INSERT INTO caracteristica VALUES (null, 'Sistema de navegacion', 'urlprueba.com');
-INSERT INTO caracteristica VALUES (null, 'Control crucero', 'urlprueba.com');
-INSERT INTO caracteristica VALUES (null, 'Detección de puntos ciegos', 'urlprueba.com');
+INSERT INTO imagenes VALUES (null, 'https://i0.wp.com/asphalt9.info/wp-content/uploads/2020/05/Lamborghini-Veneno-1.jpg?fit=1062%2C588&ssl=1', 2);
+INSERT INTO imagenes VALUES (null, 'https://i0.wp.com/asphalt9.info/wp-content/uploads/2020/05/Lamborghini-Veneno-1.jpg?fit=1062%2C588&ssl=1', 2);
+INSERT INTO imagenes VALUES (null, 'https://i0.wp.com/asphalt9.info/wp-content/uploads/2020/05/Lamborghini-Veneno-1.jpg?fit=1062%2C588&ssl=1', 2);
+INSERT INTO imagenes VALUES (null, 'https://i0.wp.com/asphalt9.info/wp-content/uploads/2020/05/Lamborghini-Veneno-1.jpg?fit=1062%2C588&ssl=1', 2);
+INSERT INTO imagenes VALUES (null, 'https://i0.wp.com/asphalt9.info/wp-content/uploads/2020/05/Lamborghini-Veneno-1.jpg?fit=1062%2C588&ssl=1', 2);
+
+INSERT INTO imagenes VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 3);
+INSERT INTO imagenes VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 3);
+INSERT INTO imagenes VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 3);
+INSERT INTO imagenes VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 3);
+INSERT INTO imagenes VALUES (null, 'https://upload.wikimedia.org/wikipedia/commons/6/60/2019_Ford_Fiesta_Active_X_Turbo_1.0_Front.jpg', 3);
+
+INSERT INTO imagenes VALUES (null, 'https://img.motoryracing.com/noticias/portada/25000/25024-n.jpg', 4);
+INSERT INTO imagenes VALUES (null, 'https://img.motoryracing.com/noticias/portada/25000/25024-n.jpg', 4);
+INSERT INTO imagenes VALUES (null, 'https://img.motoryracing.com/noticias/portada/25000/25024-n.jpg', 4);
+INSERT INTO imagenes VALUES (null, 'https://img.motoryracing.com/noticias/portada/25000/25024-n.jpg', 4);
+INSERT INTO imagenes VALUES (null, 'https://img.motoryracing.com/noticias/portada/25000/25024-n.jpg', 4);
+
+INSERT INTO caracteristicas VALUES (null, 'Transmision Automatica', '<i class="fa-solid fa-gears"></i>');
+INSERT INTO caracteristicas VALUES (null, 'Sistema de navegacion', '<FontAwesomeIcon icon="fa-solid fa-circle-location-arrow" />');
+INSERT INTO caracteristicas VALUES (null, 'Control crucero', '<FontAwesomeIcon icon="fa-solid fa-ship" />');
+INSERT INTO caracteristicas VALUES (null, 'Detección de puntos ciegos', '<FontAwesomeIcon icon="fa-solid fa-eye" />');
 
 INSERT INTO producto_caracteristica VALUES (1, 1);
 INSERT INTO producto_caracteristica VALUES (1, 2);
