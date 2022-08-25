83de8cf6820599891f8d754914fba5daccd02910
Stephania Martheyn <stephaniemartheyn@gmail.com>
2022-08-24 20:55:56 -0500

    Arregle responsive y visualizacion con diseño del menu hamburguesa

--- FrontEnd/proyecto-integrador/src/Paginas/Home/Header/Header.js
+++ FrontEnd/proyecto-integrador/src/Paginas/Home/Header/Header.js
@@ -2,12 +2,13 @@ import { useRef, useContext } from "react";
 import React from "react";
 import "./Header.css";
 import logoeasyrides from "./logoeasyrides.png";
-import { FaBars, FaTimes} from "react-icons/fa";
+//import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useInitialState } from "../../../Hooks/useInitialState";
 import AuthContext from "../../../Context/AuthContext";
-import { BotonesLoginRegister } from "./BotonesLoginRegister";
-import { Avatar } from "./Avatar";
+//import { BotonesLoginRegister } from "./BotonesLoginRegister";
+//import { Avatar } from "./Avatar";
+import menuHamburguesa from "./menuHamburguesa.svg";
 
 const Header = () => {
   const navRef = useRef();
@@ -24,21 +25,23 @@ const Header = () => {
       <Link to="/">
         <img className="header-logo" src={logoeasyrides} alt="logo" />
       </Link>
-      <nav ref={navRef} className="header-derecha">
-        {/*mostrar avatar en un solo compontente */}
-        {auth ? <Avatar/>  : <BotonesLoginRegister/>}
-        
-        {/* botones para mobile */}
-        <button
-          className="header-boton header-cerrar-boton"
-          onClick={mostrarNavBar}
-        >
-          <FaTimes />
-        </button>
-      </nav>
-      <button className="header-boton" onClick={mostrarNavBar}>
-        <FaBars />
-      </button>
+
+      {/*Desarrollo de los botones con menu hamburguesa*/}
+
+      <label  for="menu" className="nav_label">
+        <img className="menuHamburguesa" src={menuHamburguesa} alt="hamburgues" />
+      </label>
+      <input type="checkbox" id="menu" className="nav_input"></input>
+
+      <div className="nav_menu">
+        <a href="#" className="nav_item" id="Iniciar-sesion">Iniciar sesión</a>
+        <a href="#" className="nav_item" id="Crear-cuenta">Crear cuenta</a>
+      </div>
+
+
+
+
+
     </header>
   );
 };
